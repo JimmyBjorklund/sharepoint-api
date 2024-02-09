30b977e256d3c91f5c8d49c48eca110cb110c5f2
Jimmy Björklund <9413089+JimmyBjorklund@users.noreply.github.com>
2024-02-09 10:24:04 +0100

    Added dist to ignore

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SharepointClient = void 0;
-const sharepoint_client_1 = require("./sharepoint-client");
-Object.defineProperty(exports, "SharepointClient", { enumerable: true, get: function () { return sharepoint_client_1.SharepointClient; } });
-//# sourceMappingURL=index.js.map
